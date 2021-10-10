7992af4e8862b88cdf1e4aeb0837eb9539c15012
Julien <julien@ripouteau.com>
2021-10-11 01:42:54 +0200

    chore: :wrench: added eslint and prettier for vue

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,16 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
+  env: {
+    node: true,
+  },
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
   },
-  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:vue/vue3-recommended',
+  ],
   rules: {},
 }
